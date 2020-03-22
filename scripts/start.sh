f4af7cb43aee98709c9971619aaa5b067bcd6451
Giuseppe C <AvverbioPronome@users.noreply.github.com>
2020-03-22 10:44:46 +0100

    separate lf_updater from frontend, correct mailer config and adjust shell variables

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 
-echo "mailhub=${SMARTHOST_ADDRESS}" > /etc/ssmtp/ssmtp.conf
+#echo "mailhub=${SMARTHOST_ADDRESS}" > /etc/ssmtp/ssmtp.conf
 
-sleep 10 # wait for the db to startup
-PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -h db -t -d ${POSTGRES_DB} -c '\d' | cut -d \| -f 2 
-PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -h db -qt -d ${POSTGRES_DB} -c '\d' | cut -d \| -f 2 | grep -qw 'member'
+until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "${POSTGRES_HOST}" -U "${POSTGRES_USER}" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 5
+done
+
+PGPASSWORD="${POSTGRES_PASSWORD}" psql -U "${POSTGRES_USER}" -h "${POSTGRES_HOST}" -t -d "${POSTGRES_DB}" -c '\d' | cut -d \| -f 2 
+PGPASSWORD="${POSTGRES_PASSWORD}" psql -U "${POSTGRES_USER}" -h "${POSTGRES_HOST}" -qt -d "${POSTGRES_DB}" -c '\d' | cut -d \| -f 2 | grep -qw 'member'
 
 if [ $? -eq 1 ] ; then
     # table does not exists and we need to bootstrap the DB
     # $? is 1
-    PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -h db -q -d ${POSTGRES_DB} -f /opt/lf/core.sql
-    PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -h db -q -d ${POSTGRES_DB} -f /opt/lf/config_db.sql
+    PGPASSWORD="${POSTGRES_PASSWORD}" psql -U "${POSTGRES_USER}" -h "${POSTGRES_HOST}" -q -d ${POSTGRES_DB} -f /opt/lf/core.sql
+    PGPASSWORD"${POSTGRES_PASSWORD}" psql -U "${POSTGRES_USER}" -h "${POSTGRES_HOST}" -q -d ${POSTGRES_DB} -f /opt/lf/config_db.sql
 fi
 
 set -e
@@ -21,15 +25,8 @@ if [ ! -f /opt/lf/frontend/config/lfconfig.lua ] ; then
 fi
 
 if [ -z $1 ] ; then
-        /opt/lf/bin/lf_updated &
 
-        su -s /bin/bash www-data -c "
-/opt/lf/moonbridge/moonbridge \
-    /opt/lf/webmcp/bin/mcp.lua \
-    /opt/lf/webmcp/ \
-    /opt/lf/frontend/ \
-    main lfconfig
-"
+        su -s /bin/bash www-data -c "/opt/lf/moonbridge/moonbridge /opt/lf/webmcp/bin/mcp.lua /opt/lf/webmcp/ /opt/lf/frontend/ main lfconfig"
 
 else
         exec "$@"
